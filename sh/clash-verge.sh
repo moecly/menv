e3147065f1bd1523cb58579c5a2c0c1e4b1b74de
moecly <1349269087ABC@gmail.com>
2026-05-04 19:11:40 +0800

    feat: 添加了btrfs和clash-verge脚本

---
+++ sh/clash-verge.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Clash Verge service
+echo "Installing Clash Verge service..."
+sudo clash-verge-service-install
+
+
+echo "Clash Verge service installed and configured successfully."
