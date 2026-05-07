7504599757c6d934b618c6ccd14bfe93e1b56562
moecly <1349269087ABC@gmail.com>
2026-05-07 17:31:57 +0800

    feat: 删除clash-verge, 使用flclash替代

--- sh/clash-verge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install Clash Verge service
-echo "Installing Clash Verge service..."
-sudo clash-verge-service-install
-
-
-echo "Clash Verge service installed and configured successfully."
